0db797676a02c084591b6d68f202887de0516327
guptasu <guptasu@google.com>
2017-03-31 08:44:47 -0700

    f

--- GeneratedFromServiceConfig/UserWrittenMethods.ts
+++ GeneratedFromServiceConfig/UserWrittenMethods.ts
@@ -1,6 +1,6 @@
-/// <reference path="typeDefs.ts"/>
+/// <reference path="TypesFromAspectDescriptors.ts"/>
 
-/// <reference path="attribs.ts"/>
+/// <reference path="WellKnownAttribs.ts"/>
 
 
 function report(attributes: Attributes) {
@@ -9,30 +9,30 @@ function report(attributes: Attributes) {
       value: attributes.ResponseLatency !== undefined ?
           attributes.ResponseLatency :
           100,
+      service: attributes.ApiName !== undefined ? attributes.ApiName : 'one',
+      source: attributes.SourceName !== undefined ? attributes.SourceName :
+                                                    'one',
       target: attributes.TargetName !== undefined ? attributes.TargetName :
                                                     'one',
       method: attributes.ApiMethod !== undefined ? attributes.ApiMethod : 'one',
       response_code: attributes.ResponseHttpCode !== undefined ?
           attributes.ResponseHttpCode :
-          111,
-      service: attributes.ApiName !== undefined ? attributes.ApiName : 'one',
-      source: attributes.SourceName !== undefined ? attributes.SourceName :
-                                                    'one'
+          111
     })
 
     RecordRequestLatency({
       value: attributes.ResponseLatency !== undefined ?
           attributes.ResponseLatency :
           200,
+      source: attributes.SourceName !== undefined ? attributes.SourceName :
+                                                    'two',
+      target: attributes.TargetName !== undefined ? attributes.TargetName :
+                                                    'two',
       method: attributes.ApiMethod !== undefined ? attributes.ApiMethod : 'two',
       response_code: attributes.ResponseHttpCode !== undefined ?
           attributes.ResponseHttpCode :
           222,
-      service: attributes.ApiName !== undefined ? attributes.ApiName : 'two',
-      source: attributes.SourceName !== undefined ? attributes.SourceName :
-                                                    'two',
-      target: attributes.TargetName !== undefined ? attributes.TargetName :
-                                                    'two'
+      service: attributes.ApiName !== undefined ? attributes.ApiName : 'two'
     })
   }
 
@@ -41,29 +41,30 @@ function report(attributes: Attributes) {
       value: attributes.ResponseLatency !== undefined ?
           attributes.ResponseLatency :
           300,
+      response_code: attributes.ResponseHttpCode !== undefined ?
+          attributes.ResponseHttpCode :
+          333,
+      service: attributes.ApiName !== undefined ? attributes.ApiName : 'three',
       source: attributes.SourceName !== undefined ? attributes.SourceName :
                                                     'three',
       target: attributes.TargetName !== undefined ? attributes.TargetName :
                                                     'three',
       method: attributes.ApiMethod !== undefined ? attributes.ApiMethod :
-                                                   'three',
-      response_code: attributes.ResponseHttpCode !== undefined ?
-          attributes.ResponseHttpCode :
-          333,
-      service: attributes.ApiName !== undefined ? attributes.ApiName : 'three'
+                                                   'three'
     })
 
     RecordRequestCount({
       value: 400,
+      target: attributes.TargetName !== undefined ? attributes.TargetName :
+                                                    'four',
+      method: attributes.ApiMethod !== undefined ? attributes.ApiMethod :
+                                                   'four',
       response_code: attributes.ResponseHttpCode !== undefined ?
           attributes.ResponseHttpCode :
           444,
       service: attributes.ApiName !== undefined ? attributes.ApiName : 'four',
       source: attributes.SourceName !== undefined ? attributes.SourceName :
-                                                    'four',
-      target: attributes.TargetName !== undefined ? attributes.TargetName :
-                                                    'four',
-      method: attributes.ApiMethod !== undefined ? attributes.ApiMethod : 'four'
+                                                    'four'
     })
   }
 }
--- GeneratedFromServiceConfig/generatedJSFromTypeScriptCompiler.js
+++ GeneratedFromServiceConfig/generatedJSFromTypeScriptCompiler.js
@@ -23,15 +23,6 @@ var Attributes = (function () {
     function Attributes(attribs) {
         // Fill the set of attribues that are part of the call (data is available
         // inside the attribs).
-        if (attribs.Get('response.http.code')[1]) {
-            this.ResponseHttpCode = attribs.Get('response.http.code')[0];
-        }
-        if (attribs.Get('response.latency')[1]) {
-            this.ResponseLatency = attribs.Get('response.latency')[0];
-        }
-        if (attribs.Get('api.method')[1]) {
-            this.ApiMethod = attribs.Get('api.method')[0];
-        }
         if (attribs.Get('target.name')[1]) {
             this.TargetName = attribs.Get('target.name')[0];
         }
@@ -41,43 +32,52 @@ var Attributes = (function () {
         if (attribs.Get('source.name')[1]) {
             this.SourceName = attribs.Get('source.name')[0];
         }
+        if (attribs.Get('response.http.code')[1]) {
+            this.ResponseHttpCode = attribs.Get('response.http.code')[0];
+        }
+        if (attribs.Get('response.latency')[1]) {
+            this.ResponseLatency = attribs.Get('response.latency')[0];
+        }
+        if (attribs.Get('api.method')[1]) {
+            this.ApiMethod = attribs.Get('api.method')[0];
+        }
     }
     return Attributes;
 }());
 function ConstructAttributes(attr) {
     return new Attributes(attr);
 }
-/// <reference path="typeDefs.ts"/>
-/// <reference path="attribs.ts"/>
+/// <reference path="TypesFromAspectDescriptors.ts"/>
+/// <reference path="WellKnownAttribs.ts"/>
 function report(attributes) {
     if (attributes.SourceName == 'test') {
         RecordRequestCount({
             value: attributes.ResponseLatency !== undefined ?
                 attributes.ResponseLatency :
                 100,
+            service: attributes.ApiName !== undefined ? attributes.ApiName : 'one',
+            source: attributes.SourceName !== undefined ? attributes.SourceName :
+                'one',
             target: attributes.TargetName !== undefined ? attributes.TargetName :
                 'one',
             method: attributes.ApiMethod !== undefined ? attributes.ApiMethod : 'one',
             response_code: attributes.ResponseHttpCode !== undefined ?
                 attributes.ResponseHttpCode :
-                111,
-            service: attributes.ApiName !== undefined ? attributes.ApiName : 'one',
-            source: attributes.SourceName !== undefined ? attributes.SourceName :
-                'one'
+                111
         });
         RecordRequestLatency({
             value: attributes.ResponseLatency !== undefined ?
                 attributes.ResponseLatency :
                 200,
+            source: attributes.SourceName !== undefined ? attributes.SourceName :
+                'two',
+            target: attributes.TargetName !== undefined ? attributes.TargetName :
+                'two',
             method: attributes.ApiMethod !== undefined ? attributes.ApiMethod : 'two',
             response_code: attributes.ResponseHttpCode !== undefined ?
                 attributes.ResponseHttpCode :
                 222,
-            service: attributes.ApiName !== undefined ? attributes.ApiName : 'two',
-            source: attributes.SourceName !== undefined ? attributes.SourceName :
-                'two',
-            target: attributes.TargetName !== undefined ? attributes.TargetName :
-                'two'
+            service: attributes.ApiName !== undefined ? attributes.ApiName : 'two'
         });
     }
     if (attributes.SourceName == 'foo') {
@@ -85,28 +85,29 @@ function report(attributes) {
             value: attributes.ResponseLatency !== undefined ?
                 attributes.ResponseLatency :
                 300,
+            response_code: attributes.ResponseHttpCode !== undefined ?
+                attributes.ResponseHttpCode :
+                333,
+            service: attributes.ApiName !== undefined ? attributes.ApiName : 'three',
             source: attributes.SourceName !== undefined ? attributes.SourceName :
                 'three',
             target: attributes.TargetName !== undefined ? attributes.TargetName :
                 'three',
             method: attributes.ApiMethod !== undefined ? attributes.ApiMethod :
-                'three',
-            response_code: attributes.ResponseHttpCode !== undefined ?
-                attributes.ResponseHttpCode :
-                333,
-            service: attributes.ApiName !== undefined ? attributes.ApiName : 'three'
+                'three'
         });
         RecordRequestCount({
             value: 400,
+            target: attributes.TargetName !== undefined ? attributes.TargetName :
+                'four',
+            method: attributes.ApiMethod !== undefined ? attributes.ApiMethod :
+                'four',
             response_code: attributes.ResponseHttpCode !== undefined ?
                 attributes.ResponseHttpCode :
                 444,
             service: attributes.ApiName !== undefined ? attributes.ApiName : 'four',
             source: attributes.SourceName !== undefined ? attributes.SourceName :
-                'four',
-            target: attributes.TargetName !== undefined ? attributes.TargetName :
-                'four',
-            method: attributes.ApiMethod !== undefined ? attributes.ApiMethod : 'four'
+                'four'
         });
     }
 }
