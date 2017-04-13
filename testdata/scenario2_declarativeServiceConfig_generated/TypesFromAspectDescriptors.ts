771f113f7a938a814fcf4241e6e8c65e26680b17
guptasu <guptasu@google.com>
2017-04-12 19:22:10 -0700

    f

--- testdata/scenario2_declarativeServiceConfig_generated/TypesFromAspectDescriptors.ts
+++ testdata/scenario2_declarativeServiceConfig_generated/TypesFromAspectDescriptors.ts
@@ -23,27 +23,19 @@ class RequestLatency {
 }
 
 function RecordRequestCountInMyAspect1(val: RequestCount){
-    CallBackFromUserScript_go(
-        'MyAspect1', {descriptorName: 'request_count', value: val})}
+    CallBackFromUserScript_go('MyAspect1', {descriptorName: 'request_count', value: val})}
 
 function RecordRequestLatencyInMyAspect1(val: RequestLatency) {
-  CallBackFromUserScript_go(
-      'MyAspect1', {descriptorName: 'request_latency', value: val})
+    CallBackFromUserScript_go('MyAspect1', {descriptorName: 'request_latency', value: val})
 }
 
 function ConstructRequestCountForMyAspect1(attributes: Attributes) {
   return {
-    value: 1,
-        method: attributes.ApiMethod !== undefined ? attributes.ApiMethod :
-                                                     'unknown',
-        response_code: attributes.ResponseCode !== undefined ?
-        attributes.ResponseCode :
-        200,
-        service: attributes.ApiName !== undefined ? attributes.ApiName :
-                                                    'unknown',
-        source: attributes.SourceName !== undefined ? attributes.SourceName :
-                                                      'unknown',
-        target: attributes.TargetName !== undefined ? attributes.TargetName :
-                                                      'unknown'
+      value: 1,
+      method: attributes.ApiMethod !== undefined ? attributes.ApiMethod : 'unknown',
+      response_code: attributes.ResponseCode !== undefined ? attributes.ResponseCode : 200,
+      service: attributes.ApiName !== undefined ? attributes.ApiName : 'unknown',
+      source: attributes.SourceName !== undefined ? attributes.SourceName : 'unknown',
+      target: attributes.TargetName !== undefined ? attributes.TargetName : 'unknown'
   }
 }
