9c6fb23f5175339fb88b99e42410f99f0eea9fd0
Sunny Gupta <guptasu@guptasu-macbookpro.roam.corp.google.com>
2017-04-14 00:33:13 -0700

    f

--- testdata/scenario2_declarativeServiceConfig_generated/TypesFromAspectDescriptors.js
+++ testdata/scenario2_declarativeServiceConfig_generated/TypesFromAspectDescriptors.js
@@ -1,7 +1,7 @@
 //-----------------CallBack Method Declaration-----------------
 // This method gets injected at runtime. Need this declaration to make
 // TypeScript happy
-var CallBackFromUserScript_go = function (aspectName, val) { };
+var __interal__callback_fn = function (aspectName, val) { };
 //-----------------All Types Declaration-----------------
 var RequestCount = (function () {
     function RequestCount() {
@@ -14,24 +14,24 @@ var RequestLatency = (function () {
     return RequestLatency;
 }());
 function RecordRequestCountInMyAspect1(val) {
-    CallBackFromUserScript_go("MyAspect1", { descriptorName: "request_count", value: val });
+    __interal__callback_fn("MyAspect1", { descriptorName: "request_count", value: val });
 }
 function RecordRequestLatencyInMyAspect1(val) {
-    CallBackFromUserScript_go("MyAspect1", { descriptorName: "request_latency", value: val });
+    __interal__callback_fn("MyAspect1", { descriptorName: "request_latency", value: val });
 }
 function ConstructRequestCountForMyAspect1(attributes) {
     return {
         value: 1,
-        method: attributes.ApiMethod !== undefined ? attributes.ApiMethod
-            : "unknown",
-        response_code: attributes.ResponseCode !== undefined
-            ? attributes.ResponseCode
-            : 200,
         service: attributes.ApiName !== undefined ? attributes.ApiName
             : "unknown",
         source: attributes.SourceName !== undefined ? attributes.SourceName
             : "unknown",
         target: attributes.TargetName !== undefined ? attributes.TargetName
-            : "unknown"
+            : "unknown",
+        method: attributes.ApiMethod !== undefined ? attributes.ApiMethod
+            : "unknown",
+        response_code: attributes.ResponseCode !== undefined
+            ? attributes.ResponseCode
+            : 200
     };
 }
