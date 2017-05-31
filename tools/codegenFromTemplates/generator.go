893a5e1a5585dd6e873406b60937f1f7c916ef40
guptasu <guptasu@google.com>
2017-05-31 02:10:45 -0700

    Make test work for code gen

--- tools/codegenFromTemplates/generator.go
+++ tools/codegenFromTemplates/generator.go
@@ -1,31 +1,51 @@
 package main
 
 import (
-	"text/template"
 	"os"
 	"path"
+	"text/template"
+	"bytes"
 )
+
+var templates = template.Must(template.ParseFiles(
+	// TODO : Embed these files as resources.
+	"templates/TemplateToProto.tmpl.proto"))
+var templateToOutFile = map[string]string {
+	"TemplateToProto.tmpl.proto": "{{.TemplateShortName}}.Proto",
+}
 
 type Generator struct {
 	outputDirFullPath string
 }
 
-type Data struct {
-	TypeMessageName string
+func (g *Generator) createFile(genDirFullPath string, fileName string) (*os.File, error) {
+	os.MkdirAll(genDirFullPath, os.ModePerm)
+	file, err := os.Create(path.Join(genDirFullPath, fileName))
+	if err != nil {
+		return nil, err
+	}
+	return file, nil
 }
 
 func (g *Generator) generate(args []string) error {
-	// TODO : Embed these files as resources.
-	tmpl, err := template.New("TemplateToProto.tmpl.proto").ParseFiles("TemplateToProto.tmpl.proto")
-	if err != nil {
-		panic(err)
+	models := parseTemplates(args)
+	for _, model := range models {
+		for k,v := range templateToOutFile {
+
+			t, _ := template.New("tmp").Parse(v)
+			buf := bytes.NewBufferString("")
+			t.ExecuteTemplate(buf, "tmp", model)
+
+			f, err := g.createFile(g.outputDirFullPath, buf.String())
+			if err != nil {
+				panic(err)
+			}
+			err = templates.ExecuteTemplate(f, k, model)
+			if err != nil {
+				panic(err)
+			}
+		}
 	}
-	file,err := os.Create(path.Join(g.outputDirFullPath, "SAMPLEGENERATED.proto"))
-	if err != nil {
-		panic(err)
-	}
-	err = tmpl.Execute(file, Data{TypeMessageName:"MetricTemplateParam"})
-	if err != nil { panic(err) }
 
 	return nil
 }
--- tools/codegenFromTemplates/generator_test.go
+++ tools/codegenFromTemplates/generator_test.go
@@ -2,47 +2,67 @@ package main
 
 import (
 	"testing"
-	//"os"
+	"os"
 	"path/filepath"
+	"io/ioutil"
+	"os/exec"
 )
 
-func test_normal(t *testing.T, input_file string, reference_file string) {
-	test_compiler(t, input_file, reference_file, false)
+func TestMetricTemplate(t *testing.T) {
+	test(t,
+		"testdata/MetricTemplate.proto",
+		"testdata/MetricTemplate.proto.baseline")
 }
-func test_compiler(t *testing.T, input_file string, reference_file string, expect_errors bool) {
+
+func test(t *testing.T, input_file string, reference_file string) {
 
 	outputDirFullPath := "testdata/generated"
-
-	//var output_dir string
-	if expect_errors {
-		// output_file = errors_file
-		// TODO
-	} else {
-		//output_dir = outputDirFullPath
-	}
-	// remove any preexisting output files
-	//os.Remove(output_dir)
-
 	fullPathOutDir, _ := filepath.Abs(outputDirFullPath)
+	err := os.RemoveAll(fullPathOutDir)
+	if err != nil {
+		panic (err)
+	}
 	generator := Generator{outputDirFullPath:fullPathOutDir}
 
 	generator.generate([]string {input_file})
 
+	flattenedOutput := filepath.Join(outputDirFullPath, "flattenedOutput")
+	flattenDirContentToAFile(fullPathOutDir, flattenedOutput)
 
 	//os.Remove(errors_file)
-	//err = exec.Command("diff", output_file, reference_file).Run()
-	//if err != nil {
-	//	t.Logf("Diff failed: %+v", err)
-	//	t.FailNow()
-	//} else {
-	//	// if the test succeeded, clean up
-	//	os.Remove(text_file)
-	//	os.Remove(errors_file)
-	//}
+	err = exec.Command("diff", flattenedOutput, reference_file).Run()
+	if err != nil {
+		t.Logf("Diff failed: %+v", err)
+		t.FailNow()
+	} else {
+		// if the test succeeded, clean up
+		os.RemoveAll(fullPathOutDir)
+	}
 }
 
-func TestMetricTemplate(t *testing.T) {
-	test_normal(t,
-		"testdata/MetricTemplate.proto",
-		"testdata/MetricTemplate.proto.baseline")
+func flattenDirContentToAFile(dir string, outFilePath string) error {
+	searchDir := dir
+
+	fileList := []string{}
+	filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
+		if !f.IsDir() {
+			fileList = append(fileList, path)
+		}
+		return nil
+
+	})
+
+	outFile, err := os.Create(outFilePath)
+	if err != nil {
+		return err
+	}
+	for _, file := range fileList {
+		s, _ := ioutil.ReadFile(file)
+		outFile.WriteString("********* " + filepath.Base(file) + " *********\n")
+		outFile.Write(s)
+		outFile.WriteString("********* END - " + filepath.Base(file) + " *********\n\n")
+	}
+	outFile.Close()
+
+	return nil
 }
