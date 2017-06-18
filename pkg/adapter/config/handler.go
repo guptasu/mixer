896cd7d8d834ac63712cce9b5fd37e690906a906
guptasu <guptasu@google.com>
2017-06-17 22:56:45 -0700

    f

---
+++ pkg/adapter/config/handler.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"io"
+	"github.com/golang/protobuf/proto"
+)
+
+// Interface for every Handler to implement.
+type Handler interface {
+	io.Closer
+
+	// Name returns the official name of the aspects produced by this builder.
+	Name() string
+	// Description returns a user-friendly description of the aspects produced by this builder.
+	Description() string
+	DefaultConfig() proto.Message
+	ValidateConfig(proto.Message) error
+	Configure(proto.Message) error
+}
