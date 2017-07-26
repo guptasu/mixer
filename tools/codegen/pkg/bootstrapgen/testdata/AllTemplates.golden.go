50444f4f2ba6b35d3f2c98a60e77ec5fac295f1f
guptasu <guptasu@google.com>
2017-07-26 10:11:29 -0700

    f

--- tools/codegen/pkg/bootstrapgen/testdata/AllTemplates.golden.go
+++ tools/codegen/pkg/bootstrapgen/testdata/AllTemplates.golden.go
@@ -16,10 +16,54 @@
 
 package template
 
+import (
+	"github.com/gogo/protobuf/proto"
+
+	"istio.io/mixer/pkg/template"
+)
+
+var (
+	SupportedTmplInfo = map[string]template.Info{
+
+		foo_bar_mylistchecker.TemplateName: {
+			InferTypeFn:     inferTypeForList,
+			CnstrDefConfig:  &foo_bar_mylistchecker.ConstructorParam{},
+			ConfigureTypeFn: configureTypeForList,
+		},
+
+		istio_mixer_adapter_metric.TemplateName: {
+			InferTypeFn:     inferTypeForMetric,
+			CnstrDefConfig:  &istio_mixer_adapter_metric.ConstructorParam{},
+			ConfigureTypeFn: configureTypeForMetric,
+		},
+
+		istio_mixer_adapter_quota.TemplateName: {
+			InferTypeFn:     inferTypeForQuota,
+			CnstrDefConfig:  &istio_mixer_adapter_quota.ConstructorParam{},
+			ConfigureTypeFn: configureTypeForQuota,
+		},
+
+		istio_mixer_adapter_log.TemplateName: {
+			InferTypeFn:     inferTypeForLog,
+			CnstrDefConfig:  &istio_mixer_adapter_log.ConstructorParam{},
+			ConfigureTypeFn: configureTypeForLog,
+		},
+	}
+)
+
 /////////////////////// Start generated code for template List ///////////////////////
 func supportsListBuilder(hndlrBuilder adptConfig.HandlerBuilder) bool {
 	_, ok := hndlrBuilder.(foo_bar_mylistchecker.ListProcessorBuilder)
 	return ok
+}
+func inferTypeForList(cp proto.Message, tEvalFn template.TypeEvalFn) (proto.Message, error) {
+	var err error
+
+	cpb := cp.(*foo_bar_mylistchecker.ConstructorParam)
+
+	infrdType := &foo_bar_mylistchecker.Type{}
+
+	return infrdType, nil
 }
 
 /////////////////////// End generated code for template List ///////////////////////
@@ -29,6 +73,15 @@ func supportsMetricBuilder(hndlrBuilder adptConfig.HandlerBuilder) bool {
 	_, ok := hndlrBuilder.(istio_mixer_adapter_metric.MetricProcessorBuilder)
 	return ok
 }
+func inferTypeForMetric(cp proto.Message, tEvalFn template.TypeEvalFn) (proto.Message, error) {
+	var err error
+
+	cpb := cp.(*istio_mixer_adapter_metric.ConstructorParam)
+
+	infrdType := &istio_mixer_adapter_metric.Type{}
+
+	return infrdType, nil
+}
 
 /////////////////////// End generated code for template Metric ///////////////////////
 
@@ -37,6 +90,15 @@ func supportsQuotaBuilder(hndlrBuilder adptConfig.HandlerBuilder) bool {
 	_, ok := hndlrBuilder.(istio_mixer_adapter_quota.QuotaProcessorBuilder)
 	return ok
 }
+func inferTypeForQuota(cp proto.Message, tEvalFn template.TypeEvalFn) (proto.Message, error) {
+	var err error
+
+	cpb := cp.(*istio_mixer_adapter_quota.ConstructorParam)
+
+	infrdType := &istio_mixer_adapter_quota.Type{}
+
+	return infrdType, nil
+}
 
 /////////////////////// End generated code for template Quota ///////////////////////
 
@@ -44,6 +106,15 @@ func supportsQuotaBuilder(hndlrBuilder adptConfig.HandlerBuilder) bool {
 func supportsLogBuilder(hndlrBuilder adptConfig.HandlerBuilder) bool {
 	_, ok := hndlrBuilder.(istio_mixer_adapter_log.LogProcessorBuilder)
 	return ok
+}
+func inferTypeForLog(cp proto.Message, tEvalFn template.TypeEvalFn) (proto.Message, error) {
+	var err error
+
+	cpb := cp.(*istio_mixer_adapter_log.ConstructorParam)
+
+	infrdType := &istio_mixer_adapter_log.Type{}
+
+	return infrdType, nil
 }
 
 /////////////////////// End generated code for template Log ///////////////////////
