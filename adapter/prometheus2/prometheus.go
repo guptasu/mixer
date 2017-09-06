02cb3243a6f745d372c11290c6b637d21587a227
Sunny Gupta <guptasu@google.com>
2017-09-06 12:36:02 -0700

    Rename prometheus Config method to Set method

--- adapter/prometheus2/prometheus.go
+++ adapter/prometheus2/prometheus.go
@@ -355,7 +355,7 @@ func (o *obuilder) Build(cfg adapter.Config, env adapter.Env) (adapter.Handler, 
 }
 
 // ConfigureMetricHandler is to be deleted
-func (o *obuilder) ConfigureMetricHandler(types map[string]*metric.Type) error {
+func (o *obuilder) SetMetricTypes(types map[string]*metric.Type) error {
 	o.b.SetMetricTypes(types)
 	return nil
 }
